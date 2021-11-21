fd98a442a2d2f0497315e8c06cc32bca4b3cd98f
Sristi Agrawal <65845120+Sristi2208@users.noreply.github.com>
2021-11-21 14:02:32 +0530

    Add files via upload

---
+++ capstone/charRecogAPI.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from ImageDownload import Image_Download
+from recognitionfunction import recognitionfunction
+
+app = Flask(__name__)
+
+
+import time
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+
+
+
+
+
+
+@app.route('/api',methods = ["GET"])
+def recognize():
+
+    time.sleep(30)
+    Image_Download()
+    IMAGE_PATH = 'img.jpeg'
+
+    text = recognitionfunction(IMAGE_PATH)
+
+    
+    
+     
+    d={}
+    print(text)
+    d['Query'] = str(text)
+    return jsonify(d)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5758)
