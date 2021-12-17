b7fa9144b83f46bfcda9d531df1e2285b1a162e6
Sristi Agrawal <65845120+Sristi2208@users.noreply.github.com>
2021-12-17 23:17:08 +0530

    Update recognitionfunction.py

--- capstone/recognitionfunction.py
+++ capstone/recognitionfunction.py
@@ -4,9 +4,55 @@ import numpy as np
 
 
 def recognitionfunction(filename):
-    word_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
+    word_dict = {0: '0',
+ 1: '1',
+ 2: '2',
+ 3: '3',
+ 4: '4',
+ 5: '5',
+ 6: '6',
+ 7: '7',
+ 8: '8',
+ 9: '9',
+ 10: 'A',
+ 11: 'B',
+ 12: 'C',
+ 13: 'D',
+ 14: 'E',
+ 15: 'F',
+ 16: 'G',
+ 17: 'H',
+ 18: 'I',
+ 19: 'J',
+ 20: 'K',
+ 21: 'L',
+ 22: 'M',
+ 23: 'N',
+ 24: 'O',
+ 25: 'P',
+ 26: 'Q',
+ 27: 'R',
+ 28: 'S',
+ 29: 'T',
+ 30: 'U',
+ 31: 'V',
+ 32: 'W',
+ 33: 'X',
+ 34: 'Y',
+ 35: 'Z',
+ 36: 'a',
+ 37: 'b',
+ 38: 'd',
+ 39: 'e',
+ 40: 'f',
+ 41: 'g',
+ 42: 'h',
+ 43: 'n',
+ 44: 'q',
+ 45: 'r',
+ 46: 't'}
 
-    model = load_model('model_hand.h5')
+    model = load_model('charrecognition_new.h5')
     img = cv2.imread(filename)
     img_copy = img.copy()
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
